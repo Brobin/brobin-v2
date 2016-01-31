c2e1ad8b157d1dbf3c287a624f17cceb137e6aef
Brobin <tobinjbrown@gmail.com>
2016-01-31 15:50:14 -0600

    added some unit tests to test functionality of navigation and blog categories

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from blog.models import Category, Post
 
-# Create your tests here.
+
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        programming = Category.objects.create(title='Programming')
+        python = Category.objects.create(title='Python', parent=programming)
+        django = Category.objects.create(title='Django', parent=python)
+
+    def test_category_save_slugify(self):
+        name = 'Programming'
+        programming = Category.objects.get(title=name)
+        self.assertEqual(programming.slug, slugify(name))
+
+    def test_category_children(self):
+        programming = Category.objects.get(title='Programming')
+        self.assertEqual(len(programming.get_children()), 3)
--- navigation/tests.py
+++ navigation/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from navigation.models import MenuItem, SubMenuItem
 
-# Create your tests here.
+
+class NavigationTestCase(TestCase):
+
+    def setUp(self):
+        MenuItem.objects.create(title='Projects', link='/projects', order=2)
+        MenuItem.objects.create(title='Admin', link='/admin', order=3)
+        blog_menu = MenuItem.objects.create(title='Blog', link='/blog', order=1)
+
+        SubMenuItem.objects.create(title='Something1', link='/blog/something1', order=1, menu=blog_menu)
+        SubMenuItem.objects.create(title='Something2', link='/blog/something2', order=2, menu=blog_menu)
+        SubMenuItem.objects.create(title='Something3', link='/blog/something3', order=3, menu=blog_menu)
+
+    def test_sub_menu_items(self):
+        blog = MenuItem.objects.get(title='Blog')
+        self.assertTrue(blog.has_sub_items)
+
+    def test_correct_ordering(self):
+        blog = MenuItem.objects.get(title='Blog')
+        order = 0
+        for item in blog.sub_menu_items:
+            self.assertTrue(item.order > order)
+            order = item.order
+
+    def test_menu_sub_items(self):
+        blog = MenuItem.objects.get(title='Blog')
+        self.assertEqual(len(blog.sub_menu_items), 3)

27dfbc0e7a8b7fe8d29e01e47bbd62814a81e893
Brobin <tobinjbrown@gmail.com>
2016-01-31 23:10:38 -0600

    remove sidebar and move it to a sitewide footer

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,14 +5,15 @@ from blog.models import Post, Category
 register = template.Library()
 
 
-@register.inclusion_tag('blog/tags/_recent_posts.html')
-def recent_posts():
+@register.inclusion_tag('blog/tags/_blog_recent.html')
+def blog_recent():
     return {'recent': Post.objects.order_by('-created')[:5]}
 
 
 @register.inclusion_tag('blog/tags/_blog_categories.html')
 def blog_categories():
     return {'categories': Category.objects.all()}
+
 
 @register.inclusion_tag('blog/tags/_blog_archive.html')
 def blog_archive():

2ea95e7c196e5c3a8ec6bdb2544087319493b872
Brobin <tobinjbrown@gmail.com>
2016-02-02 21:44:45 -0600

    update code style, fix the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 source ~/venv/bin/activate
 echo "activating virtualenv"
 git pull
-python manage.py collectstatic
+python manage.py collectstatic --noinput
+cp -r ~/brobin.me/brobin/brobin/static/ ~/brobin.me/public
 python manage.py migrate
 cd ..
 rm tmp/restart.txt

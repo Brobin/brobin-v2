20c620670d1780e186e66fe7e3cc5eff0871a9d9
Brobin <tobinjbrown@gmail.com>
2016-02-03 14:12:28 -0600

    add an rss and atom feed to the blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from blog.feeds import BlogPostRssFeed, BlogPostAtomFeed
 from blog import views
 
 
 urlpatterns = [
     url(r'^$', views.blog),
+    url(r'^feed/rss/$', BlogPostRssFeed()),
+    url(r'^feed/atom/$', BlogPostAtomFeed()),
     url(r'^search/$', views.search, name="search"),
     url(r'^([a-zA-Z\-]*)/$', views.category),
     url(r'^archive/([0-9]{4})/$', views.archive),

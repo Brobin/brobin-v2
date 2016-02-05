9b6aa85fe22a2c5b97c2995f59c7f6d416d3c91f
Brobin <tobinjbrown@gmail.com>
2016-02-05 09:17:20 -0600

    add tests for all of the blog routes, verify 200 responses

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from datetime import date, datetime
 from blog.models import Category, Post
 
 
@@ -27,3 +29,39 @@ class BlogTestCase(TestCase):
         post = Post.objects.get(title='test')
         queryset = Post.visible_posts.all()
         self.assertTrue(post not in queryset)
+
+
+class BlogRequestTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        programming = Category.objects.create(title='Programming')
+        user = User.objects.create(username='tobin')
+        Post.objects.create(title='test', content='', visible=True,
+                            author=user, category=programming,
+                            created=datetime.now())
+
+    def test_blog_index(self):
+        response = self.client.get(reverse('blog_index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_category(self):
+        url = reverse('blog_category', args=['programming'])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_archive(self):
+        year = date.today().year
+        url = reverse('blog_archive', args=[year])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_post(self):
+        url = reverse('blog_post', kwargs={'slug': 'test'})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_search(self):
+        url = reverse('blog_search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from blog import views
 
 
 urlpatterns = [
-    url(r'^$', views.blog),
+    url(r'^$', views.blog, name='blog_index'),
     url(r'^feed/rss/$', BlogPostRssFeed()),
     url(r'^feed/atom/$', BlogPostAtomFeed()),
-    url(r'^search/$', views.search, name="search"),
-    url(r'^([a-zA-Z\-]*)/$', views.category),
-    url(r'^archive/([0-9]{4})/$', views.archive),
-    url(r'^[0-9]{4}/[0-9]{2}/(?P<slug>.*)/$', views.blog_post),
+    url(r'^search/$', views.search, name='blog_search'),
+    url(r'^([a-zA-Z\-]*)/$', views.category, name='blog_category'),
+    url(r'^archive/([0-9]{4})/$', views.archive, name='blog_archive'),
+    url(r'^[0-9]{4}/[0-9]{2}/(?P<slug>.*)/$', views.blog_post, name='blog_post'),
 ]

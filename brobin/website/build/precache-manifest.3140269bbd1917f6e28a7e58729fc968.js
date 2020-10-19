5baa957aba4d88b9ac186731ed4880a5b02fc5e5
Tobin Brown <tobin@bulubox.com>
2020-10-19 11:56:46 -0500

    build

---
+++ brobin/website/build/precache-manifest.3140269bbd1917f6e28a7e58729fc968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb4fbd920dbfc52e0dd0b7e1c3f8ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a14f1c0cef85ee4c7734",
+    "url": "/static/css/main.972ae740.chunk.css"
+  },
+  {
+    "revision": "b62034e4989f14cc1511",
+    "url": "/static/js/2.b88f1536.chunk.js"
+  },
+  {
+    "revision": "b83114502dbf883a5db8cd7c0a22f370",
+    "url": "/static/js/2.b88f1536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14f1c0cef85ee4c7734",
+    "url": "/static/js/main.ed2aa907.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
